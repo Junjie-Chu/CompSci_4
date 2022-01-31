456a1ec5add866de317e3f143a8f01bac794f96f
Junjie-Chu <65893273+Junjie-Chu@users.noreply.github.com>
2022-01-31 15:48:31 +0100

    Update and rename utilize_general_CommunityDetectionMethods.py to utilize_general_CommunityDetectionMethods_weighted.py

---
+++ CommunityAnalysis/utilize_general_CommunityDetectionMethods_weighted.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import numpy as np
+import math as m
+import time 
+from pycowview.data import csv_read_FA
+from pycowview.manipulate import unique_cows
+from pycowview.metrics import interaction_time
+import networkx as nx
+%matplotlib inline
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import itertools
+import os
+import community
+from collections import defaultdict,Counter
+import progressbar
+import random
+import itertools 
+from sklearn.metrics.cluster import normalized_mutual_info_score
+from sklearn.metrics.cluster import adjusted_mutual_info_score
+from sklearn.metrics import f1_score
+
+def community_Louvain(i,G):
+    #print('This is the result of day %d'%(i+1))
+    
+    # Nodes whose degree is zero
+    nodes_removed = [node for node,degree in dict(G.degree()).items() if degree == 0]
+    # If you want to remove them
+    #G.remove_nodes_from(nodes_removed)
+    #print(len(nodes_removed),'nodes whose degree is zero are removed')
+    
+    # Louvain algorithm
+    partition = community.best_partition(G,weight = 'weight',randomize=False)
+    num_communities = max(partition.values())
+    
+    # create a dict object:{community1:[nodelist],community2:[nodelist],......}
+    # community1:[nodelist] is a tuple
+    communities_Louvain = defaultdict(list) 
+    for k, v in partition.items():
+        communities_Louvain[v].append(k)    
+    np.save('./community/Louvain_weighted/Day_%d_Louvain_weighted_communities.npy'%(i+1), communities_Louvain)
+    
+    #read the dict from file
+    #communities_Louvain = np.load('./community/Louvain/Day_%d_Louvain_Unweighted_communities.npy', allow_pickle='TRUE')
+    #print(communities_Louvain)
+
+    num_communities = max(partition.values())
+    len_Louvain = len(communities_Louvain)
+    #print('max No. of community:',num_communities)
+    #print('num of communities:',len_Louvain)
+    #print('Modularity:',nx.algorithms.community.quality.modularity(G,communities_Louvain.values()))
+
+    # Colormap for plotting
+    color_Louvain = 0
+    random.seed(7)
+    total_colors = list(mpl.colors.get_named_colors_mapping())
+    total_colors.remove('black')
+    color_map_Louvain = random.sample(total_colors,len_Louvain)
+    
+    '''
+    # Plot the figure
+    plt.figure(figsize=(15, 15))  # image size
+    pos = nx.fruchterman_reingold_layout(G, scale = 1) # position of nodes
+    degree_dict = dict(G.degree())
+    nx.draw_networkx(G, pos, node_size=5,width=0.05, alpha=1, with_labels=False)
+    for community_Louvain in communities_Louvain.items():
+        
+        node_list = community_Louvain[1]
+        edge_list = list(itertools.chain.from_iterable([list(G.edges(node)) for node in node_list]))
+        label_list = {}
+        for node in node_list:
+            #set the node name as the key and the label as its value 
+            label_list[node] = node 
+        community_degree_dict = {key: value for key, value in degree_dict.items() if key in node_list}
+        node_size_list = [d*20 for d in community_degree_dict.values()]
+        
+        nx.draw_networkx_nodes(G, pos , nodelist = node_list, node_size = node_size_list, node_color = color_map_Louvain[color_Louvain],alpha = 0.7)
+        nx.draw_networkx_edges(G, pos , edgelist = edge_list, edge_color = color_map_Louvain[color_Louvain],alpha = 0.2)
+        nx.draw_networkx_labels(G, pos, label_list, font_size = 8, font_color = color_map_Louvain[color_Louvain],alpha = 0.7)
+        color_Louvain += 1
+    plt.savefig('./community/Louvain_weighted/Day%d_weighted_Community.png'%(i+1))
+    #plt.show()
+    '''
+    return nodes_removed, communities_Louvain, partition
