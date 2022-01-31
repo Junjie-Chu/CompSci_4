5486912975c974ba06934f22043fec3560f5ed0f
Junjie-Chu <65893273+Junjie-Chu@users.noreply.github.com>
2022-01-31 15:42:24 +0100

    Create utilize_general_weighted.py

---
+++ CommunityAnalysis/utilize_general_weighted.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import numpy as np
+import math as m
+import time 
+from pycowview.data import csv_read_FA
+from pycowview.manipulate import unique_cows
+from pycowview.metrics import interaction_time
+import networkx as nx
+%matplotlib inline
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import itertools
+import os
+import community
+from collections import defaultdict,Counter
+import progressbar
+import random
+import itertools 
+from sklearn.metrics.cluster import normalized_mutual_info_score
+from sklearn.metrics.cluster import adjusted_mutual_info_score
+from sklearn.metrics import f1_score
+
+# Weighted version!
+# This function will get the path of each csv file
+def findAllFile(base):
+    for root, ds, fs in os.walk(base):
+        for f in fs:
+            if f.endswith('.csv'):
+                fullname = os.path.join(root, f)
+                yield fullname
+
+# Input is the folders where the time matrix and cowlist are saved
+# Output is a list which consists of 14 dictionaries
+# The structure of dictionary:Cowlist,TimeMatrix,AajacencyMatrix_binary,Unweighted_Graph
+def time_matrix_to_graph(tm_folder,cl_folder):
+    dict_list = []
+    i = 0
+    tmlist = list(findAllFile(tm_folder))
+    tmlist.sort()
+    cllist = list(findAllFile(cl_folder))
+    cllist.sort()
+    for tm,cl in zip(tmlist,cllist):
+        # print(tm,cl)
+        # Get the path of csv
+        # get cowlist
+        cowlist = np.loadtxt(cl,delimiter=",").astype(int)
+        # load original time matrix from csv and process it to be an adjacency Matrix
+        OM = np.asmatrix(np.loadtxt(tm,delimiter=","))
+        
+        # Get unweighted adjacency matrix(binary)
+        # init adjacency matrix
+        #AM = np.zeros((OM.shape))
+        # set the threshold to be 30 minutes(1800 seconds)
+        #epsilon = 1800
+        # just consider if there is an edge between two cows, the edge is unweighted
+        #AM[OM >= epsilon] = 1
+        #AM[OM < epsilon] = 0 
+        #np.fill_diagonal(AM,0)
+        # Get graph from AM, no-direct and no-weight graph
+        #G_AM_temp = nx.from_numpy_matrix(AM,parallel_edges=False,create_using = nx.Graph())
+        
+        # Get weighted adjacency matrix
+        # load original matrix from csv and process it to be an adjacency Matrix
+        AM = np.zeros((OM.shape))
+        # set the threshold to be 30 minutes(1800 seconds)
+        epsilon = 1800
+        # consider if there is an edge between two cows, the edge is weighted
+        maxnr=np.amax(OM)
+        AM=np.where(OM<=epsilon,0,(OM-epsilon)/(maxnr-epsilon))
+        np.fill_diagonal(AM,0)
+        # Get graph from AM, no-direct and weighted graph
+        G_AM_temp = nx.from_numpy_matrix(AM,parallel_edges=False,create_using = nx.Graph())
+        
+        # Make sure the order of cowlist is the same as the row name!
+        print('Shape of matrix:',AM.shape)
+        print('number of nodes in graph',len(G_AM_temp),'length of cowlist',len(cowlist))
+        mapping = dict(zip(G_AM_temp, cowlist))
+        #print(mapping)
+        # Rename the nodes
+        G_AM = nx.relabel_nodes(G_AM_temp, mapping)
+        
+        # Get the dict of the collection(CL,TM,AM_weighted,Graph)
+        data_dict = dict(CL=cowlist,TM=OM,AM_weighted=AM,Graph=G_AM)
+        print('Document No.',i)
+        print('TM path:',tm,'CL path:',cl)
+        i = i + 1
+        dict_list.append(data_dict)
+
+    print('The length of the list: ',len(dict_list))
+    return dict_list
